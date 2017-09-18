70b2b4b15cb1524318c08e9a18b117b6e7f0b6dd
Ada Avery <2561504+SquircleSpace@users.noreply.github.com>
2017-09-18 09:32:10 -0700

    Make the rainbow fancier

--- rainbow.cpp
+++ rainbow.cpp
@@ -14,11 +14,14 @@
 namespace LED {
     class RainbowCylinder : public Effect {
         using Super = Effect;
-        static constexpr float CYCLE_TIME = 10;
-        static constexpr float PIXEL_GAP = 1.0 / 15;
-        static constexpr float STRIP_GAP = 1.0 / 10;
+        float cycleTime_ = 5;
+        float pixelGap_ = 1.0 / 15;
+        float stripGap_ = 1.0 / 10;
+        bool alternateDirections_ = true;
         float timeOffset_ = 0;
         float frameCost_ = 0;
+        float value_ = 0.75;
+        float saturation_ = 1;
     public:
         void shader(Vec3 &, const PixelInfo &) const override;
         void postProcess(const Vec3 &, const PixelInfo &) override;
@@ -30,10 +33,11 @@ namespace LED {
 void LED::RainbowCylinder::shader(Vec3 &rgb, const PixelInfo &pixel) const {
     const int strip = std::round(pixel.getNumber("strip"));
     const int offset = std::round(pixel.getNumber("offset"));
-    const float pixel_offset_factor = STRIP_GAP * strip + PIXEL_GAP * offset;
-    const float time_factor = timeOffset_ / CYCLE_TIME;
-    const float cyclePortion = std::fmod(time_factor + pixel_offset_factor, 1);
-    hsv2rgb(rgb, cyclePortion, 1, .75);
+    const int direction = (!alternateDirections_ || strip % 2 == 0 ? 1 : -1);
+    const float pixel_offset_factor = (stripGap_ * strip) + (pixelGap_ * offset);
+    const float time_factor = timeOffset_ / cycleTime_;
+    const float cyclePortion = std::fmod(time_factor + pixel_offset_factor * direction, 1);
+    hsv2rgb(rgb, cyclePortion, saturation_, value_);
 }
 
 void LED::RainbowCylinder::postProcess(const Vec3 &rgb, const PixelInfo &pixel) {
@@ -43,7 +47,7 @@ void LED::RainbowCylinder::postProcess(const Vec3 &rgb, const PixelInfo &pixel) 
 }
 
 void LED::RainbowCylinder::beginFrame(const FrameInfo &frame) {
-    timeOffset_ = std::fmod(timeOffset_ + frame.timeDelta, CYCLE_TIME);
+    timeOffset_ = std::fmod(timeOffset_ + frame.timeDelta, cycleTime_);
     frameCost_ = 0;
     Super::beginFrame(frame);
 }
